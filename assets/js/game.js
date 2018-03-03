3923b841fdef0d5ad2ed7a74b34a07ad76a26e3e
Justin Buckley <justin.buckley@apexmediagrp.com>
2018-03-03 10:26:25 -0800

    minor changes
    @jmbuckley

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
     // timer that counts down and also displays the countdown on the page
 var seconds = 10
 var interval = setInterval(function() {
@@ -57,17 +56,13 @@ var interval = setInterval(function() {
     ];
   
     function buildQuiz() {
-      // we'll need a place to store the HTML output
+      // STORE HTML OUTPUT
       const output = [];
-  
       // for each question...
       myQuestions.forEach((currentQuestion, questionNumber) => {
-        // we'll want to store the list of answer choices
+        // ANSWER CHOICES
         const answers = [];
-  
-        // and for each available answer...
         for (letter in currentQuestion.answers) {
-          // ...add an HTML radio button
           answers.push(
             `<label>
                <input type="radio" name="question${questionNumber}" value="${letter}">
@@ -76,8 +71,6 @@ var interval = setInterval(function() {
              </label>`
           );
         }
-  
-        // add this question and its answers to the output
         output.push(
           `<div class="slide">
              <div class="question"> ${currentQuestion.question} </div>
@@ -85,54 +78,35 @@ var interval = setInterval(function() {
            </div>`
         );
       });
-  
-      // finally combine our output list into one string of HTML and put it on the page
+
       quizContainer.innerHTML = output.join("");
     }
   
     function showResults() {
-      // gather answer containers from our quiz
       const answerContainers = quizContainer.querySelectorAll(".answers");
-  
-      // keep track of user's answers
       let numCorrect = 0;
-  
-      // for each question...
       myQuestions.forEach((currentQuestion, questionNumber) => {
-        // find selected answer
         const answerContainer = answerContainers[questionNumber];
         const selector = `input[name=question${questionNumber}]:checked`;
         const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-  
-        // if answer is correct
         if (userAnswer === currentQuestion.correctAnswer) {
-          // add to the number of correct answers
           numCorrect++;
-  
-          // color the answers green
           answerContainers[questionNumber].style.color = "lightgreen";
         } else {
-          // if answer is wrong or blank
-          // color the answers red
           answerContainers[questionNumber].style.color = "red";
         }
       });
-  
-      // show number of correct answers out of total
       resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
     }
-  
     function showSlide(n) {
       slides[currentSlide].classList.remove("active-slide");
       slides[n].classList.add("active-slide");
       currentSlide = n;
-      
       if (currentSlide === 0) {
         previousButton.style.display = "none";
       } else {
         previousButton.style.display = "inline-block";
       }
-      
       if (currentSlide === slides.length - 1) {
         nextButton.style.display = "none";
         submitButton.style.display = "inline-block";
@@ -141,30 +115,21 @@ var interval = setInterval(function() {
         submitButton.style.display = "none";
       }
     }
-  
     function showNextSlide() {
       showSlide(currentSlide + 1);
     }
-  
     function showPreviousSlide() {
       showSlide(currentSlide - 1);
     }
-  
     const quizContainer = document.getElementById("quiz");
     const resultsContainer = document.getElementById("results");
     const submitButton = document.getElementById("submit");
-  
-    // display quiz right away
     buildQuiz();
-  
     const previousButton = document.getElementById("previous");
     const nextButton = document.getElementById("next");
     const slides = document.querySelectorAll(".slide");
     let currentSlide = 0;
-  
     showSlide(0);
-  
-    // on submit, show results
     submitButton.addEventListener("click", showResults);
     previousButton.addEventListener("click", showPreviousSlide);
     nextButton.addEventListener("click", showNextSlide);
